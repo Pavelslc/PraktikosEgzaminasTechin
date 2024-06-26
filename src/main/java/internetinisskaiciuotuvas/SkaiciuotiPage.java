1e3ef7c61be673469320c0db2a62492efa578a7c
Pavelslc <salc.pavel1@gmail.com>
2024-06-26 12:42:25 +0300

    add waitForToLoadSkaiciuoti()

--- src/main/java/internetinisskaiciuotuvas/SkaiciuotiPage.java
+++ src/main/java/internetinisskaiciuotuvas/SkaiciuotiPage.java
@@ -1,15 +1,17 @@
 package internetinisskaiciuotuvas;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class SkaiciuotiPage extends BasePage{
+public class SkaiciuotiPage extends SkaiciuotuvasPage{
 
     public SkaiciuotiPage(WebDriver driver) {
         super(driver);
     }
 
-    public SkaiciuotiPage waitForToLoadSkaiciuoti(){
+    public SkaiciuotiPage waitForToLoadSkaiciuoti() {
         wait.until(ExpectedConditions.titleIs("Skaičiuoti"));
         return this;
     }
