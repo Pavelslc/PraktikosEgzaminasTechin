726a7a890a1e97262e097dfdb8f8654e7aed4b22
Pavelslc <salc.pavel1@gmail.com>
2024-06-26 13:18:49 +0300

    add register sequence for positive test

--- src/test/java/internetinisskaiciuotuvas/LoginTest.java
+++ src/test/java/internetinisskaiciuotuvas/LoginTest.java
@@ -8,29 +8,47 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LoginTest extends BaseTest{
     private LoginPage loginPage;
     private SkaiciuotuvasPage skaiciuotuvasPage;
+    private RegisterPage registerPage;
 
 
     @BeforeEach
     void loginTestSetup() {
         loginPage = new LoginPage(driver);
         skaiciuotuvasPage = new SkaiciuotuvasPage(driver);
+        registerPage = new RegisterPage(driver);
     }
 
     @Test
     void loginTest (){
-        String username = "User001";
+        // create user
+        String username = RandomNameGenerator.generateRandomName();
         String password = "password123.";
-        loginPage
+        String passwordConfirm = password;
+        loginPage.clickLinkRegister();
+        registerPage
+                .sendInputUsername(username)
+                .sendInputPassword(password)
+                .sendInputPasswordConfirm(passwordConfirm)
+                .clickButtonSubmit();
+        skaiciuotuvasPage.waitForPageToLoadSkaiciuotuvas();
+        skaiciuotuvasPage.clickLinkLogOut();
+
+        // login
+        loginPage.waitForToLoadPrisijungimas()
                 .sendInputUsername(username)
                 .sendInputPassword(password)
                 .clickButtonSubmit();
         skaiciuotuvasPage.waitForPageToLoadSkaiciuotuvas();
+
+        // assert
         assertEquals("Skaičiuotuvas", driver.getTitle(), "page title does not match");
+
+        // logout
         skaiciuotuvasPage.clickLinkLogOut();
     }
     @Test
     void loginNegativeTest (){
-        String username = "User001";
+        String username = "a";
         String password = "";
         loginPage
                 .sendInputUsername(username)
