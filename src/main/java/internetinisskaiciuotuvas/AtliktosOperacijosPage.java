88dae60756a77512bf458de03b7c1da4d0618a57
Pavelslc <salc.pavel1@gmail.com>
2024-06-26 13:21:25 +0300

    add minor change

--- src/main/java/internetinisskaiciuotuvas/AtliktosOperacijosPage.java
+++ src/main/java/internetinisskaiciuotuvas/AtliktosOperacijosPage.java
@@ -1,4 +1,17 @@
 package internetinisskaiciuotuvas;
 
-public class AtliktosOperacijosPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AtliktosOperacijosPage extends SkaiciuotuvasPage{
+@FindBy (xpath = "//table[@class='table table-striped']//tr[last()]/td[4]")
+    private WebElement paskutinioIrasoSuma;
+
+    public AtliktosOperacijosPage(WebDriver driver) {
+        super(driver);
+    }
+    public String getPaskutinioIrasoSuma(){
+        return paskutinioIrasoSuma.getText();
+    }
 }
