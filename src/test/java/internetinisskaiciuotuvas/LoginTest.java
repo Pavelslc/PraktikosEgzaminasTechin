297cb039604883edba6d307b1d00a4e59c997c23
Pavelslc <salc.pavel1@gmail.com>
2024-06-26 10:59:44 +0300

    copmplete positive loginTest

--- src/test/java/internetinisskaiciuotuvas/LoginTest.java
+++ src/test/java/internetinisskaiciuotuvas/LoginTest.java
@@ -3,24 +3,28 @@ package internetinisskaiciuotuvas;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LoginTest extends BaseTest{
     private LoginPage loginPage;
+    private SkaiciuotuvasPage skaiciuotuvasPage;
+
 
     @BeforeEach
     void loginTestSetup() {
         loginPage = new LoginPage(driver);
+        skaiciuotuvasPage = new SkaiciuotuvasPage(driver);
     }
 
-//    @Test
-//    void loginTest (){
-//        String username = "User001";
-//        String password = "password123.";
-//        String passwordConfirm = password;
-//        loginPage.clickLinkRegister();
-//        registerPage
-//                .sendInputUsername(username)
-//                .sendInputPassword(password)
-//                .sendInputPasswordConfirm(passwordConfirm)
-//                .clickButtonSubmit();
-//    }
+    @Test
+    void loginTest (){
+        String username = "User001";
+        String password = "password123.";
+        loginPage
+                .sendInputUsername(username)
+                .sendInputPassword(password)
+                .clickButtonSubmit();
+        skaiciuotuvasPage.waitForPageToLoadSkaiciuotuvas();
+        assertEquals("Skaičiuotuvas", driver.getTitle(), "page title does not match");
+    }
 }
