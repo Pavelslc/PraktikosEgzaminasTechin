3fdf11bbbccf125f1f318f1356c48e1d306fab96
Pavelslc <salc.pavel1@gmail.com>
2024-06-26 12:40:58 +0300

    add isDisplayedErrorLogin

--- src/main/java/internetinisskaiciuotuvas/LoginPage.java
+++ src/main/java/internetinisskaiciuotuvas/LoginPage.java
@@ -5,34 +5,50 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class LoginPage extends BasePage{
-    @FindBy (css = "[name=\"username\"]")
+public class LoginPage extends BasePage {
+    @FindBy(css = "[name=\"username\"]")
     private WebElement inputUsername;
-    @FindBy (css = "[name=\"password\"]")
+    @FindBy(css = "[name=\"password\"]")
     private WebElement inputPassword;
-    @FindBy (css = "[type='submit']")
+    @FindBy(css = "[type='submit']")
     private WebElement buttonSubmit;
-    @FindBy (css = "h4.text-center a")
+    @FindBy(css = "h4.text-center a")
     private WebElement lingRegister;
+    @FindBy(css = ".form-group.has-error")
+    private WebElement errorLogin;
+
     public LoginPage(WebDriver driver) {
         super(driver);
     }
+
     public LoginPage sendInputUsername(String username) {
         wait.until(ExpectedConditions.visibilityOf(inputUsername));
         actions.click(inputUsername).sendKeys(username).perform();
         return this;
     }
-    public LoginPage sendInputPassword (String password) {
+
+    public LoginPage sendInputPassword(String password) {
         wait.until(ExpectedConditions.visibilityOf(inputPassword));
         actions.click(inputPassword).sendKeys(password).perform();
         return this;
     }
-    public void clickButtonSubmit () {
+
+    public void clickButtonSubmit() {
         wait.until(ExpectedConditions.elementToBeClickable(buttonSubmit));
         actions.click(buttonSubmit).perform();
     }
-    public void clickLinkRegister(){
-    wait.until(ExpectedConditions.elementToBeClickable(lingRegister));
+
+    public void clickLinkRegister() {
+        wait.until(ExpectedConditions.elementToBeClickable(lingRegister));
         lingRegister.click();
+    }
+
+    public LoginPage waitForToLoadPrisijungimas() {
+        wait.until(ExpectedConditions.titleIs("Prisijungimas"));
+        return this;
+    }
+
+    public boolean IsDisplayedErrorLogin() {
+        return errorLogin.isDisplayed();
     }
 }
